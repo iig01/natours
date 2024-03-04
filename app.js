46e6337248bee5f5657b2ebc654a5e656d6bc4c6
iig01 <iisgg20@gmail.com>
2024-03-05 00:28:48 +0500

    controllers & routes added

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// MIDDLEWARES
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestedTime = new Date().toISOString();
+  next();
+});
+
+// ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// SERVER
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-``
