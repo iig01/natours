76c1ba35759f9b63eaa9d175b73b116d80b1b550
iig01 <iisgg20@gmail.com>
2024-03-05 22:51:09 +0500

    some bugs fixed

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const {
-  getAllTours,
-  createTour,
-  getTour,
-  updateTour,
-  deleteTour,
-} = require('./../controllers/tourController');
+const tourController = require('./../controllers/tourController');
 
 // ROUTES
 const router = express.Router();
-router.route('/').get(getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+router.param('id', tourController.checkID);
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
